740f3d8aa3f16d681478f3fede49591a15d391b1
“lalitsaini85” <lalit.saini_cs19@gla.ac.in>
2023-02-02 17:17:05 +0530

    string immutable

---
+++ src/OOPs/string/StringBasic.java
@@ -0,0 +1,15 @@
+package OOPs.string;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Lalit Saini (lalitsaini85)
+ * Date: 2/2/2023
+ * Time: 5:15 PM
+ * File Name : StringBasic.java
+ */
+public class StringBasic {
+    public static void main(String[] args) {
+        String str = "name";
+        String str1 = "name";    // both variables are pointing to same memory location.
+    }
+}
